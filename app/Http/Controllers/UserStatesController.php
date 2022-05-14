2816e12f7c612fb01be07bc4716ce70c1c1acee4
bagav1 <bragaru@hotmail.es>
2022-05-13 19:36:40 -0500

    GET User States

--- app/Http/Controllers/UserStatesController.php
+++ app/Http/Controllers/UserStatesController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UserStates;
 use Illuminate\Http\Request;
 
 class UserStatesController extends Controller
 {
-    //
+    public function getAll()
+    {
+        try {
+            return response()->json([
+                'status' => true,
+                'data' => UserStates::get()
+            ], 200);
+        } catch (Exception $ex) {
+            return response()->json([
+                'status' => false,
+                'error' => $ex->getMessage()
+            ], 400);
+        }
+    }
 }
